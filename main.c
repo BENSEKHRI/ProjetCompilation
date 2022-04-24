a3ea648271b5c02eefbbb2292ca8fb03aaa692d0
BENSEKHRI <lounisbensekhri@gmail.com>
2022-04-24 15:56:05 +0200

    input

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    if (!yyparse()) {                      // call to the parsing (and lexing) function
-        printf("\nParsing:: syntax OK\n"); // reached if parsing follows the grammar
+extern FILE* yyin;
+
+int main(int argc, char const *argv[]) {
+    yyin = fopen(argv[1], "r");
+
+    if (yyin != NULL) {     // Avec un fichier en argument 
+        if (!yyparse(yyin)) {                  // call to the parsing (and lexing) function
+            printf("\nParsing:: syntax OK\n"); // reached if parsing follows the grammar
+        }
+    } else {                // Sans fichier en argument
+        if (!yyparse()) {                      // call to the parsing (and lexing) function
+            printf("\nParsing:: syntax OK\n"); // reached if parsing follows the grammar
+        }
     }
+    
     exit(EXIT_SUCCESS);
 }
