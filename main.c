f66994cc99887f79ded302f32c574af0478735b7
BENSEKHRI <lounisbensekhri@gmail.com>
2022-04-24 15:58:55 +0200

    input

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main(int argc, char const *argv[]) {
         if (!yyparse(yyin)) {                  // call to the parsing (and lexing) function
             printf("\nParsing:: syntax OK\n"); // reached if parsing follows the grammar
         }
+        fclose(yyin); // On ferme le fichier qui a été ouvert
     } else {                // Sans fichier en argument
         if (!yyparse()) {                      // call to the parsing (and lexing) function
             printf("\nParsing:: syntax OK\n"); // reached if parsing follows the grammar
